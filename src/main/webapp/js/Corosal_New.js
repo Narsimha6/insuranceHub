839e6532308b0bafa826632ff305fec52352d478
Sourabh Gupta <sourabh.a.gupta@capgemini.com>
2017-03-23 15:40:32 +0530

    Slider changes and P and C final page

--- src/main/webapp/js/Corosal_New.js
+++ src/main/webapp/js/Corosal_New.js
@@ -44,4 +44,23 @@ jQuery(document).ready(function ($) {
             };
 
             var jssor_html5_AdWords_slider = new $JssorSlider$("jssor_html5_AdWords", jssor_html5_AdWords_options);
+            
+            
+            /*Top Button Scroll*/
+            
+            var offset = 220;
+    		var duration = 500;
+    		jQuery(window).scroll(function() {
+    			if (jQuery(this).scrollTop() > offset) {
+    				jQuery('.back-to-top').fadeIn(duration);
+    			} else {
+    				jQuery('.back-to-top').fadeOut(duration);
+    			}
+    		});
+    		
+    		jQuery('.back-to-top').click(function(event) {
+    			event.preventDefault();
+    			jQuery('html, body').animate({scrollTop: 0}, duration);
+    			return false;
+    		})
         });
